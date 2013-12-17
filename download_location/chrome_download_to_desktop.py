f38e65632c4616e70a0d167cfcd078f8fcbda8df
Robotto <mark@moore.dk>
2013-12-17 16:08:04 +0100

    forgot the fucking crunchbang

--- download_location/chrome_download_to_desktop.py
+++ download_location/chrome_download_to_desktop.py
@@ -14,6 +14,7 @@
 import sys
 import json
 
+
 download_directory = '/home/user/Desktop'
 chrome_preferences = '/home/.skjult/.config/google-chrome/Default/Preferences'
 #download_directory = '/home/robotto/Desktop'
