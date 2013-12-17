063771e547ec20b62d661be8c4010a606345ef07
superuser <superuser@BibOS2.(none)>
2013-12-17 12:44:36 +0100

    fixed stupid sleepy mistake .. i forgot the .config folder

--- chrome_autostart/chrome_to_.autostart.sh
+++ chrome_autostart/chrome_to_.autostart.sh
@@ -15,14 +15,14 @@ fi
 if [ "$1" == "on" ]
 then
     echo "Adding chrome to autostart"
-    mkdir /home/.skjult/.autostart
-    printf "[Desktop Entry]\nType=Application\nExec=google-chrome\nHidden=false\nNoDisplay=false\nX-GNOME-Autostart-enabled=true\nName[en_US]=Chrome\nName=Chrome\nComment[en_US]=run the Google-chrome webbrowser at startup\nComment=run the Google-chrome webbrowser at startup\nName[en]=Chrome\n" > /home/.skjult/.autostart/chrome.desktop
+    mkdir /home/.skjult/.config/autostart
+    printf "[Desktop Entry]\nType=Application\nExec=google-chrome\nHidden=false\nNoDisplay=false\nX-GNOME-Autostart-enabled=true\nName[en_US]=Chrome\nName=Chrome\nComment[en_US]=run the Google-chrome webbrowser at startup\nComment=run the Google-chrome webbrowser at startup\nName[en]=Chrome\n" > /home/.skjult/.config/autostart/chrome.desktop
 
     echo "Done."
 
 else
     echo "removing chrome from autostart"
-    rm /home/.skjult/.autostart/chrome.desktop
+    rm /home/.skjult/.config/autostart/chrome.desktop
     echo "Done."
 fi
 
