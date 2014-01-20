987d791e0c9f878cec60b4dc4cff3fabe6241196
Robotto <mark@moore.dk>
2014-01-20 11:40:17 +0100

    updated .java folder script to correspond with new java version and use gateway server instead of robottobox

--- java/get_netbank_enabled_dot_java_folder.sh
+++ java/get_netbank_enabled_dot_java_folder.sh
@@ -2,9 +2,13 @@
 
 #getting a ~/.java folder that has already allowed netbank...
 
+echo "Removing old .java folder..."
 cd /home/.skjult
 rm -rf .java
-wget 62.212.66.171/dot_java.tgz
-tar -xvf dot_java.tgz
-rm dot_java.tgz
-echo "Done. Bye. :)"
+echo "Fecthing new tarball from gateway server..."
+wget 172.16.12.220/dotJava.tar.bz2
+echo "unpacking.."
+tar -xvf dotJava.tar.bz2
+echo "cleaning up..."
+rm dotJava.tar.bz2
+echo "Done. You should reboot now... Bye. :)"
