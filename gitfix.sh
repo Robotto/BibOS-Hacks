339988c951c281823af55613e55a3a73df7aab09
Publikum <user@a-itk-test-pc3.(none)>
2014-01-20 12:49:23 +0100

    using curl instead of wget because i like the verbose output

---
+++ gitfix.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git config --global user.name "robotto"
+git config --global user.email mark@moore.dk
+
--- java/get_netbank_enabled_dot_java_folder.sh
+++ java/get_netbank_enabled_dot_java_folder.sh
@@ -7,7 +7,8 @@ cd /home/.skjult
 rm -rf .java
 rm -rf .oces2
 echo "Fecthing new tarball from gateway server..."
-wget 172.16.12.220/dotJava.tar.bz2
+#wget 172.16.12.220/dotJava.tar.bz2
+curl http://172.16.12.220/dotJava.tar.bz2 > dotJava.tar.bz2 -vvv
 echo "unpacking.."
 tar -xvf dotJava.tar.bz2
 echo "cleaning up..."
