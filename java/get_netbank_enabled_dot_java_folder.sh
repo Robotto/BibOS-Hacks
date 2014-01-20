bd96a60718645d28cc3dc5b17d3254eefe45dbe3
robotto <mark@moore.dk>
2014-01-20 14:01:56 +0100

    reverted to robottobox

--- java/get_netbank_enabled_dot_java_folder.sh
+++ java/get_netbank_enabled_dot_java_folder.sh
@@ -2,13 +2,18 @@
 
 #getting a ~/.java folder that has already allowed netbank...
 
+echo "I AM:"
+whoami
+echo "I AM IN:"
+pwd
+
 echo "Removing old java folders..."
 cd /home/.skjult
 rm -rf .java
 rm -rf .oces2
 echo "Fecthing new tarball from gateway server..."
-#wget 172.16.12.220/dotJava.tar.bz2
-curl http://172.16.12.220/dotJava.tar.bz2 > dotJava.tar.bz2 -vvv
+wget http://62.212.66.171/dotJava.tar.bz2
+#curl http://172.16.12.220:80/dotJava.tar.bz2 -vvv -o dotJava.tar.bz2
 echo "unpacking.."
 tar -xvf dotJava.tar.bz2
 echo "cleaning up..."
