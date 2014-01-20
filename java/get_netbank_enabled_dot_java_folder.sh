dcb970da1406cfa5b954e2c39d98c5ab7422d1b8
robotto <mark@moore.dk>
2014-01-20 15:02:11 +0100

    reverted to robottobox, added .pki folder to the tarball

--- java/get_netbank_enabled_dot_java_folder.sh
+++ java/get_netbank_enabled_dot_java_folder.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #getting a ~/.java folder that has already allowed netbank...
+#also fetches .oces2/ and .pki
 
 echo "I AM:"
 whoami
@@ -11,11 +12,11 @@ echo "Removing old java folders..."
 cd /home/.skjult
 rm -rf .java
 rm -rf .oces2
+rm -rf .pki
 echo "Fecthing new tarball from gateway server..."
-wget http://62.212.66.171/dotJava.tar.bz2
-#curl http://172.16.12.220:80/dotJava.tar.bz2 -vvv -o dotJava.tar.bz2
+wget http://62.212.66.171/bibOS_java_folders.tar.bz2
 echo "unpacking.."
-tar -xvf dotJava.tar.bz2
+tar -xvf bibOS_java_folders.tar.bz2
 echo "cleaning up..."
-rm dotJava.tar.bz2
+rm bibOS_java_folders.tar.bz2
 echo "Done. You should reboot now... Bye. :)"
