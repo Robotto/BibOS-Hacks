bc1d5cb4c5f1052afc06a12e39a0b9fcd4582059
robotto <mark@moore.dk>
2014-02-04 13:23:33 +0100

    doing screensaver stuff... still need to find a proper line replacement command

--- screensaver/set_screensaver.sh
+++ screensaver/set_screensaver.sh
@@ -3,38 +3,47 @@
 if [ $# -ne 1 ]
 then
     echo "usage: $(basename $0) <'screensaver text'>"
-    echo "Assuming you meant to turn it off..."
+    echo "turn off text with $(basename $0) <off>"
+    exit 1
 fi
-echo "Installing xscreensaver.."
-apt-get install xscreensaver xscreensaver-gl-extra xscreensaver-data-extra -y --force-yes
-echo "Removing gmome-screensaver"
-apt-get remove gnome-screensaver
+#echo "Installing xscreensaver.."
+#apt-get install xscreensaver xscreensaver-gl-extra xscreensaver-data-extra -y --force-yes
+#echo "Removing gmome-screensaver"
+#apt-get remove gnome-screensaver
 
 #get original .xscreensaver settings file:
-cd /home/skjult/
+cd /home/.skjult/
+#cd /home/user/
 rm .xscreensaver
-wget 
+wget http://62.212.66.171/BibOS/.xscreensaver
 
-#remove previous entry with simple check for "textLiteral" string
-sudo sed -i_bak -e /textLiteral/d /home/.skjult/.xscreensaver
 
 
 if [ "$1" == "off" ]
 then
-	echo
+
+	#remove previous entry with simple check for "textLiteral" string
 	echo "Removing text from screensaver"
-    #it was done on line 11...
+
+	#WRONG:
+
+	#sed -i_bak -e /gltext/d /home/.skjult/.xscreensaver
+	sed -i_bak -e /gltext/d /home/user/.xscreensaver
+
 	echo "Done, enjoy your new text-less screensaver :)"
 	exit 0
 else
 	echo
-	echo "Setting screensaver text.."
+	echo "Setting new screensaver text.."
 
 	echo
 	echo "editing /home/.skjult/.xscreensaver..."
-	sed -i 's|^textFile:.*$|textLiteral:    NOGET\ntextFile: |' /home/.skjult/.xscreensaver
+
+	#WRONG!
+#	sed -i 's|^textFile:.*$|textLiteral:    $1\ntextFile: |' /home/.skjult/.xscreensaver
+	sed -i 's|^programs.*$|programs:/\ngltext -root -text $1|' /home/user/.xscreensaver
 	echo
-	echo "Done. Enjoy your chrome enabled startup :)"
+	echo "Done. Enjoy your new screensaver text :)"
 	echo
 	exit 0
 fi
