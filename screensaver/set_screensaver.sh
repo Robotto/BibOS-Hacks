a170d1b8c13a10cbbb84ca59ff6b1c7f5d04e703
Robotto <mark@moore.dk>
2014-02-04 17:19:52 +0100

    done.. almost

--- screensaver/set_screensaver.sh
+++ screensaver/set_screensaver.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-userdir="/home/user"
+userdir="/home/robotto/BIBOS_HACK/screensaver/string_playground"
+#userdir="/home/.skjult"
 
 if [ $# -ne 1 ]
 then
     echo "usage: $(basename $0) <'screensaver text'>"
-    echo "Turn off with $(basename $0) <off>"    
+    echo "Turn off with $(basename $0) <off>"
     exit 1
 fi
 
-echo "Wget'ing default .xscreensaver file.."
-cd $userdir
-rm .xscreensaver
-wget http://62.212.66.171/bibOS_DOT_xscreensaver -O .xscreensaver
+echo "Removing old .xscreensaver file, if present"
+rm $userdir/.xscreensaver
 echo "Done."
-
 
 if [ "$1" == "off" ]
 then
@@ -24,17 +22,20 @@ then
 	exit 0
 else
 
-    	echo "Adding xscreensaver to autostart"
-    	mkdir $userdir/.config/autostart
-    	printf "[Desktop Entry]\nType=Application\nExec=xscreensaver -nosplash\nHidden=false\nNoDisplay=false\nX-GNOME-Autostart-enabled=true\nName[en_US]=xscreensaver\nName=xscreensaver\nComment[en_US]=run the xscreensaver at startup\nComment=run the xscreensaver at startup\nName[en]=xscreensaver\n" > $userdir/.config/autostart/xscreensaver.desktop
-    	echo "Done."
+    echo "Wget'ing default .xscreensaver file.."
+    wget http://62.212.66.171/bibOS_DOT_xscreensaver -O $userdir/.xscreensaver
+
+    echo "Adding xscreensaver to autostart"
+    mkdir $userdir/.config/autostart
+    printf "[Desktop Entry]\nType=Application\nExec=xscreensaver -nosplash\nHidden=false\nNoDisplay=false\nX-GNOME-Autostart-enabled=true\nName[en_US]=xscreensaver\nName=xscreensaver\nComment[en_US]=run the xscreensaver at startup\nComment=run the xscreensaver at startup\nName[en]=xscreensaver\n" > $userdir/.config/autostart/xscreensaver.desktop
+    echo "Done."
 	echo
 	echo "Editing $userdir/.xscreensaver... "
-	#WRONG!
-	#remove current gltext line:
-	sed -i "/gltext/d" $userdir/.xscreensaver
-#	sed -i "/programs:/c\n  GL:                           gltext -root -text '$1'          \n\|" $userdir/.xscreensaver
-	echo
+
+    newline=''' GL:                           gltext -root -text "'$1'"        \\n\\'''
+
+	sed -i "/gltext/c$newline" $userdir/.xscreensaver
+  	echo
 	echo "Done. Enjoy your new screensaver text :)"
 	exit 0
 fi
