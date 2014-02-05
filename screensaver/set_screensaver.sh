b8fc332e91151edb94351aabba0ce5d175fe16c0
Robotto <mark@moore.dk>
2014-02-05 09:13:35 +0100

    now with screensaver timeout as an option

--- screensaver/set_screensaver.sh
+++ screensaver/set_screensaver.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#userdir="/home/robotto/BIBOS_HACK/screensaver/string_playground"
-userdir="/home/.skjult"
+userdir="/home/robotto/BIBOS_HACK/screensaver/string_playground"
+#userdir="/home/.skjult"
 
-if [ $# -ne 1 ]
+if [ $# -ne 2 ]
 then
-    echo "usage: $(basename $0) <'screensaver text'>"
-    echo "Turn off with $(basename $0) <off>"
+    echo "usage: $(basename $0) <'screensaver text'> <'timeout in minutes'>"
+    echo "Turn off with $(basename $0) <off> <whatever>"
     exit 1
 fi
 
@@ -14,10 +14,11 @@ echo "Removing old .xscreensaver file, if present"
 rm $userdir/.xscreensaver
 echo "Done."
 
+
 if [ "$1" == "off" ]
 then
 	echo "Removing xscreensaver from autostart"
-	rm $userdir/.config/autostart/xscreensaver.desktop
+#	rm $userdir/.config/autostart/xscreensaver.desktop
 	echo "Done, enjoy your new text-less screensaver :)"
 	exit 0
 else
@@ -27,14 +28,26 @@ else
 
     echo "Adding xscreensaver to autostart"
     #mkdir $userdir/.config/autostart
-    printf "[Desktop Entry]\nType=Application\nExec=xscreensaver -nosplash\nHidden=false\nNoDisplay=false\nX-GNOME-Autostart-enabled=true\nName[en_US]=xscreensaver\nName=xscreensaver\nComment[en_US]=run the xscreensaver at startup\nComment=run the xscreensaver at startup\nName[en]=xscreensaver\n" > $userdir/.config/autostart/xscreensaver.desktop
+ #   printf "[Desktop Entry]\nType=Application\nExec=xscreensaver -nosplash\nHidden=false\nNoDisplay=false\nX-GNOME-Autostart-enabled=true\nName[en_US]=xscreensaver\nName=xscreensaver\nComment[en_US]=run the xscreensaver at startup\nComment=run the xscreensaver at startup\nName[en]=xscreensaver\n" > $userdir/.config/autostart/xscreensaver.desktop
     echo "Done."
 	echo
 	echo "Editing $userdir/.xscreensaver... "
 
-    newline=''' GL:                           gltext -root -text "'$1'"        \\n\\'''
+    #minutes=$((${2}-2))
+    if [ $(($2)) -lt 10 ]
+    then
+        minutes="0$2"
+        echo $minutes
+    else minutes="$2"
+        echo $minutes
+fi
 
-	sed -i "/gltext/c$newline" $userdir/.xscreensaver
+    gltextline=''' GL:                           gltext -root -text "'$1'"        \\n\\'''
+    timeoutline='''timeout: 0:'$minutes':00'''
+
+	sed -i "/gltext/c$gltextline" $userdir/.xscreensaver
+    sed -i "/timeout/c$timeoutline" $userdir/.xscreensaver
+
   	echo
 	echo "Done. Enjoy your new screensaver text :)"
 	exit 0
