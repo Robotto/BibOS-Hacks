9f7da5cfa8651340c0374ec138eb4bbddb807c3b
Robotto <mark@moore.dk>
2014-02-05 19:23:24 +0100

    added kiosk mode autostart with URL as second param

---
+++ chrome_autostart/autostart_chrome_in_kioskmode.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# This script will add the chrome.desktop file to /home/.skjult/.autostart which will make chrome run at boot in kiosk mode and go to the specified URL
+
+
+# Expect exactly two input parameters
+
+if [ $# -ne 2 ]
+then
+    echo "usage: $(basename $0) <on/off> <url>"
+    echo ""
+    exit -1
+fi
+
+if [ "$1" == "on" ]
+then
+    echo "Setting up kiosk-mode Chrome to autostart and go to $2"
+    mkdir /home/.skjult/.config/autostart
+    printf "[Desktop Entry]\nType=Application\nExec= google-chrome --kiosk $2 --full-screen\nHidden=false\nNoDisplay=false\nX-GNOME-Autostart-enabled=true\nName[en_US]=Chrome\nName=Chrome\nComment[en_US]=run the Google-chrome webbrowser at startup\nComment=run the Google-chrome webbrowser at startup\nName[en]=Chrome\n" > /home/.skjult/.config/autostart/chrome.desktop
+
+    echo "Done."
+
+else
+    echo "removing chrome from autostart"
+    rm /home/.skjult/.config/autostart/chrome.desktop
+    echo "Done."
+fi
+
+exit 0
+
