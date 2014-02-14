c5a0a6746b6a5f0424b6aa36a857c90ae60237a8
Robotto <mark@moore.dk>
2014-02-14 09:06:30 +0100

    proper MAC addresses

--- Minecraft_installer/MACspoofing/robotto.sh
+++ Minecraft_installer/MACspoofing/robotto.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ifconfig eth0 down
-#ifconfig eth0 hw ether 40:61:86:78:a3:0a
-ifconfig eth0 hw ether 40:61:86:63:25:84
+ifconfig eth0 hw ether 40:61:86:78:a3:0a
+#ifconfig eth0 hw ether 40:61:86:63:25:84
 ifconfig eth0 up
