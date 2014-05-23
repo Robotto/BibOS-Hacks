49274907dbc8a9ab884bd1de5ab7a829cfe5898a
robotto <mark@moore.dk>
2014-05-23 11:08:52 +0200

    fixed for automation

--- pipelight/pipelight_installer.sh
+++ pipelight/pipelight_installer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo add-apt-repository ppa:pipelight/stable -y
 sudo apt-get update
-sudo apt-get install --install-recommends pipelight-multi -y --force-yes
-sudo pipelight-plugin --update
+sudo apt-get install --install-recommends pipelight-multi -y
+sudo pipelight-plugin -y --update
 
 # Type in 'Y' to accept the license of Silverlight and all required modules
-sudo pipelight-plugin --enable silverlight
+sudo pipelight-plugin -y --enable silverlight
