2de79804a4754bcd25495574a6aade80b40c3702
Robotto <mark@moore.dk>
2014-05-23 10:39:24 +0200

    added pipelight installer script, initial version

---
+++ pipelight/pipelight_installer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo add-apt-repository ppa:pipelight/stable -y --force-yes
+sudo apt-get update
+sudo apt-get install --install-recommends pipelight-multi -y --force-yes
+sudo pipelight-plugin --update
+
+# Type in 'Y' to accept the license of Silverlight and all required modules
+sudo pipelight-plugin --enable silverlight
