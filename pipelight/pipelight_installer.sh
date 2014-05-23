a03000c3e5e76d7e84144340d198b49483de282e
Robotto <mark@moore.dk>
2014-05-23 10:44:30 +0200

    edit pipelight installer

--- pipelight/pipelight_installer.sh
+++ pipelight/pipelight_installer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo add-apt-repository ppa:pipelight/stable -y --force-yes
+sudo add-apt-repository ppa:pipelight/stable -y
 sudo apt-get update
 sudo apt-get install --install-recommends pipelight-multi -y --force-yes
 sudo pipelight-plugin --update
