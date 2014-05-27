83bffddbb6b2a83fdd2b4de662c908300f42bad2
robotto <mark@moore.dk>
2014-05-27 12:31:48 +0200

    added vnc starter script

---
+++ vnc_start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ $# -ne 1 ]
+then
+	echo "you forgot to enter the IP og the host!"
+	echo "try again."
+	exit -1
+fi
+
+HOST=$1
+ssh user@$HOST -L 5900:localhost:5900 "x11vnc -display :0 -noxdamage -auth guess -nopw"
